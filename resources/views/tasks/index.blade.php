a2e79bade0590e2e9ab0f45ad7fffc1bd00bff12
bijelivuk <bijelivuk@gmail.com>
2018-05-30 11:16:08 +0200

    first commit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,32 @@
+
+
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="panel panel-default">
+    <div class="panel-body">
+      @include('common.errors')
+
+
+      <form action="{{ url('task') }}" method="post" class="form-horizontal">
+        <div class="form-group">
+          <label for="" class="col-sm-6 control-label" >Task</label>
+
+          <div class="col-sm-6">
+            <input type="text" name="name" id="name" class="form-control">
+          </div>
+
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-3 col-sm-6">
+            <button type="submit" class="btn btn-default"> Add task </button>
+          </div>
+        </div>
+      {{ csrf_field() }}
+      </form>
+
+    </div>
+  </div>
+
+@endsection
